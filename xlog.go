a5463697804f0fcaa54df48fbfaa906986d2ce86
sunhongtao <sunhongtao@xiaohulu.com>
2022-01-14 20:12:02 +0800

    输出流类型

--- xlog.go
+++ xlog.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	rotate "github.com/lestrrat-go/file-rotatelogs"
 	"github.com/sirupsen/logrus"
+	"os"
 	"time"
 )
 
@@ -11,26 +12,51 @@ const (
 	FORMAT_JSON = 1
 	FORMAT_TXT  = 2
 )
+const (
+	STD  = 1
+	FILE = 2
+	HTTP = 3
+)
 
+/**
+FORMAT_JSON = 1
+FORMAT_TXT  = 2
+*/
 type FORMAT_LOG int
+
+/**
+STD=1
+FILE=2
+HTTP=3
+*/
+type OUT_TYPE int
 
 type XLogger interface {
 }
 
 type XLog struct {
 	*logrus.Logger
+	outType OUT_TYPE
 }
 
 func NewXLogger() *XLog {
-	return &XLog{logrus.New()}
+	lgs := logrus.New()
+	lgs.SetFormatter(&logrus.JSONFormatter{})
+	lgs.SetOutput(os.Stdout)
+	return &XLog{Logger: lgs}
+}
+func (log *XLog) BuildOutType(out OUT_TYPE) {
+	log.outType = out
 }
 func (log *XLog) BuildFile(prefix string, RotationTime time.Duration) *XLog {
-	fileName := fmt.Sprintf("%v_log_%v", prefix, "%Y%m%d%H%M")
-	rl, err := rotate.New(fileName, rotate.WithRotationTime(RotationTime))
-	if err != nil {
-		panic(err)
+	if log.outType == FILE {
+		fileName := fmt.Sprintf("%v_log_%v", prefix, "%Y%m%d%H%M")
+		rl, err := rotate.New(fileName, rotate.WithRotationTime(RotationTime))
+		if err != nil {
+			panic(err)
+		}
+		log.SetOutput(rl)
 	}
-	log.SetOutput(rl)
 	return log
 }
 
